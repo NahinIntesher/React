c402d52f6635cb9e11ec92c5bc57949d82942130
Assassin318554 <nahinsikdar123@gmail.com>
2024-05-10 10:21:37 +0600

    a

--- React With Ador/basicProject/src/App.jsx
+++ React With Ador/basicProject/src/App.jsx
@@ -4,33 +4,37 @@ import React from "react";
 import Box from "./Components/box.jsx";
 
 const items = [
-  {code: 1101, name: 'English I', color: "red"},
-  {code: 1102, name: 'OPP', color: "blue"},
-  {code: 1103, name: 'SPL', color: "purple"},
-  {code: 2203, name: 'DLD', color: "green"}
+  { code: 1101, name: "English I", color: "red" },
+  { code: 1102, name: "OPP", color: "blue" },
+  { code: 1103, name: "SPL", color: "purple" },
+  { code: 2203, name: "DLD", color: "green" },
 ];
 export default function App(obj) {
   return (
     <div>
-      {
-        items.map(function(el){
-          return <Box key={el.code} color={el.color} name={el.name} code={el.code}></Box>
-        })
-      }
+      {items.map(function (el) {
+        return (
+          <Box
+            key={el.code}
+            color={el.color}
+            name={el.name}
+            code={el.code}
+          ></Box>
+        );
+      })}
     </div>
   );
-    // if(obj.isLogged == true) {
-    //     return <Logged/>
-    // }
-    // else {
-    //     return <Unlogged/>
-    // }
-    // return(
-    //   <>
-    //     {
-    //       obj.isLogged && <Logged/>
-    //     }
-    //   </>
-    // );
-
+  // if(obj.isLogged == true) {
+  //     return <Logged/>
+  // }
+  // else {
+  //     return <Unlogged/>
+  // }
+  // return(
+  //   <>
+  //     {
+  //       obj.isLogged && <Logged/>
+  //     }
+  //   </>
+  // );
 }

52d1e5198ab0df8280fd7d7b100b2ff27437fc29
Assassin318554 <nahinsikdar123@gmail.com>
2024-05-30 22:17:31 +0600

    a

--- React With Ador/basicProject/src/App.jsx
+++ React With Ador/basicProject/src/App.jsx
@@ -1,17 +1,15 @@
 import React from "react";
-// import SimpleTimer from "./Components/BasicComponents/SetState";
-// import Clock from "./Components/BasicComponents/Clock";
-// import TwoWayDataBinding from "./Components/BasicComponents/InputForm";
-// import BoilingVerdict from "./Components/BoilingVerdict/BoilingVerdict";
-// import Calculate from "./Components/BoilingVerdict/Calculate";
-// import App from "./Components/ReactComposition/App";
-// import ClickCounter from "./Components/HigherOrderComponent/ClickCounter";
-// import HoverCounter from "./Components/HigherOrderComponent/HoverCounter";
-// import Counter from "./Components/RenderProps/Counter";
-// import ClickCounter from "./Components/RenderProps/ClickCounter";
-// import HoverCounter from "./Components/RenderProps/HoverCounter";
-
-
+import SimpleTimer from "./Components/BasicComponents/SetState";
+import Clock from "./Components/BasicComponents/Clock";
+import TwoWayDataBinding from "./Components/BasicComponents/InputForm";
+import BoilingVerdict from "./Components/BoilingVerdict/BoilingVerdict";
+import Calculate from "./Components/BoilingVerdict/Calculate";
+import App from "./Components/ReactComposition/App";
+import ClickCounter from "./Components/HigherOrderComponent/ClickCounter";
+import HoverCounter from "./Components/HigherOrderComponent/HoverCounter";
+import Counter from "./Components/RenderProps/Counter";
+import ClickCounter from "./Components/RenderProps/ClickCounter";
+import HoverCounter from "./Components/RenderProps/HoverCounter";
 
 function App() {
   return (
@@ -43,9 +41,6 @@ function App() {
           <HoverCounter count={count} incrementCount={incrementCount} />
         )}
       </Counter>*/}
-
-
-
     </div>
   );
 }

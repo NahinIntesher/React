b9c34f3869c0fc703659c99464e17b47354b2137
Assassin318554 <nahinsikdar123@gmail.com>
2024-06-05 23:43:26 +0600

    a

--- React With Ador/basicProject/src/Components/Hookss/Todo.jsx
+++ React With Ador/basicProject/src/Components/Hookss/Todo.jsx
@@ -3,10 +3,23 @@ import { useState } from "react";
 function Todo() {
   const [todo, setTodo] = useState();
   const [warning, setWarning] = useState();
-  return(
-    <div>
-      
 
+  handleInput = (e) => {
+    const input = e.target.value;
+    const updatedWarning = input.includes(".js")
+      ? "You need JS skill to complete the task. Do you have it?"
+      : null;
+    setTodo(inputValue);
+    setWarning(updatedWarning);
+  };
+
+  return (
+    <div>
+      <p>Todo</p>
+      <p>
+        <textarea name="todo" value={todo} onChange={handleInput} />
+      </p>
+      <h2>{warning || "Good Choice!"}</h2>
     </div>
   );
 }

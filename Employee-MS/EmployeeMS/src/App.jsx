c6ead463a0dc5ca1ae7df6e63b5600f3592b2445
Assassin318554 <nahinsikdar123@gmail.com>
2024-07-05 06:15:10 +0600

    a

---
+++ Employee-MS/EmployeeMS/src/App.jsx
@@ -0,0 +1,11 @@
+import Login from './Components/Login';
+function App() {
+  return (
+    <div>
+      <h1>Employee Management System</h1>
+      <Login />
+    </div>
+  );
+}
+
+export default App;

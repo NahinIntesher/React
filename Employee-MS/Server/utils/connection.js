c537952fb82402835a8bee61b5949a95fc34461a
Assassin318554 <nahinsikdar123@gmail.com>
2024-07-09 00:01:21 +0600

    a

--- Employee-MS/Server/utils/connection.js
+++ Employee-MS/Server/utils/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "company",
+  database: "EmployeeMS",
 });
 
 module.exports = connection;

cc86ee832727e0f9741f18eb7d74f715d83ebbed
Assassin318554 <nahinsikdar123@gmail.com>
2024-11-22 19:05:45 +0600

    a

--- SwiftMart/src/Components/Footer/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function Footer() {
-  return (
-    <h1>This is Footer</h1>
-  );
-}
-export default Footer;
--- SwiftMart/src/Components/Header/Header.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function Header() {
-  return (
-    <h1>This is header</h1>
-  );
-}
-export default Header;
